65f6566c5fd05c4d2731109cfd08e7e905e06cc1
Editor T <j17e4eo@icloud.com>
2025-09-16 21:50:20 +0900

    Add files via upload

---
+++ mythof5/src/main/java/me/j17e4eo/mythof5/Mythof5.java
@@ -0,0 +1,17 @@
+package me.j17e4eo.mythof5;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Mythof5 extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
